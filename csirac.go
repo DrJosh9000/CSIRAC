d8f830307cc3c175a50ee6818d82e782af8009f9
Josh Deprez <josh.deprez@gmail.com>
2021-12-31 15:33:26 +1100

    Progress

--- csirac.go
+++ csirac.go
@@ -29,9 +29,11 @@ var (
 // CSIRAC represents the entire CSIRAC machine state.
 type CSIRAC struct {
 	// Registers (originally implemented using mercury delay-line memory).
-	A, B, C Word
-	H       Word // H only has 10 bits
-	D       [16]Word
+	A Word     // Supports +,-,<<,>>,AND,XOR,NAND
+	B Word     // Supports >>,*
+	C Word     // Supports >>,+,-
+	H Word     // 10 bits - stores upper or lower half word
+	D [16]Word // Each supports >>,+,-
 
 	// Sequence register (a.k.a. program counter/instruction pointer).
 	S Word
@@ -56,12 +58,27 @@ type CSIRAC struct {
 	Printer, TapePunch, Loudspeaker func(Word)
 }
 
+// Step executes one instruction.
+func (c *CSIRAC) Step() {
+	inst := c.K
+	src := c.ReadSource(inst)
+	// Three things could happen depending on the destination:
+	// 1) The destination is neither S nor K. Increment S and then fetch the
+	//    next instruction as normal here.
+	// 2) The destination is S. WriteDest updates S and K again.
+	// 3) The destination modifies K (PK). Since it doesn't modify S, the
+	//    next instruction is always the one fetched here.
+	c.S++
+	c.K = c.M[c.S]
+	c.WriteDest(inst, src)
+}
+
 // ReadSource reads the source field from the instruction k, and uses that to
 // read a word from a variety of sources.
-func (c *CSIRAC) ReadSource(k Word) Word {
-	switch k.Source() {
+func (c *CSIRAC) ReadSource(inst Word) Word {
+	switch inst.Source() {
 	case 0: // n M - Read from main store
-		return c.M[k.Hi()]
+		return c.M[inst.Hi()]
 	case 1: // I - Read input switches
 		return c.I
 	case 2: // NA - Read switch register 1
@@ -100,11 +117,11 @@ func (c *CSIRAC) ReadSource(k Word) Word {
 	case 16: // RC - Read the C register shifted right
 		return c.C >> 1
 	case 17: // n D - Read from one of the D registers
-		return c.D[k.Hi()]
+		return c.D[inst.Hi()]
 	case 18: // n SD - Read the sign bit of one of the D registers
-		return c.D[k.Hi()].Sign()
+		return c.D[inst.Hi()].Sign()
 	case 19: // n RD - Read one of the D registers shifted right
-		return c.D[k.Hi()] >> 1
+		return c.D[inst.Hi()] >> 1
 	case 20: // Z - Read zero.
 		return 0
 	case 21: // HL - Read the H register as a lower half
@@ -120,15 +137,15 @@ func (c *CSIRAC) ReadSource(k Word) Word {
 		return 1
 	case 26: // n K - Read the upper half of the instruction (a literal)
 		// TODO: check
-		return k.Hi()
+		return inst.Hi()
 	case 27: // n MA - Read disk 1
-		return c.MA[k.Hi()]
+		return c.MA[inst.Hi()]
 	case 28: // n MB - Read disk 2
-		return c.MB[k.Hi()]
+		return c.MB[inst.Hi()]
 	case 29: // n MC - Read disk 3
-		return c.MC[k.Hi()]
+		return c.MC[inst.Hi()]
 	case 30: // n MD - Read disk 4
-		return c.MD[k.Hi()]
+		return c.MD[inst.Hi()]
 	case 31: // PS - Read a number with 1 in the sign bit
 		return 1 << 19
 	}
@@ -137,81 +154,88 @@ func (c *CSIRAC) ReadSource(k Word) Word {
 
 // WriteDest reads the dest field from the instruction k, and uses that to
 // write the word v to a variety of sources.
-func (c *CSIRAC) WriteDest(k, v Word) error {
-	switch k.Dest() {
+func (c *CSIRAC) WriteDest(inst, src Word) error {
+	switch inst.Dest() {
 	case 0: // n M - Write to main store
-		c.M[k.Hi()] = v
+		c.M[inst.Hi()] = src
 	case 1: // Q - Set binary or decimal input
 		// "in Melb no-op"
 	case 2: // OT - Write to console printer
-		c.Printer(v)
+		c.Printer(src)
 	case 3: // OP - Write to tape punch
-		c.TapePunch(v)
+		c.TapePunch(src)
 	case 4: // A - Write to A register
-		c.A = v
+		c.A = src
 	case 5: // PA - Add into A register
-		c.A = (c.A + v) & allBits
+		c.A = (c.A + src) & allBits
 	case 6: // SA - Subtract into A register
-		c.A = (c.A - v) & allBits
+		c.A = (c.A - src) & allBits
 	case 7: // CA - AND with A register
-		c.A &= v
+		c.A &= src
 	case 8: // DA - XOR with A register
-		c.A ^= v
+		c.A ^= src
 	case 9: // NA - NAND with A register
-		c.A = ^(c.A & v) & allBits
+		c.A = ^(c.A & src) & allBits
 	case 10: // P - Loudspeaker
-		c.Loudspeaker(v)
+		c.Loudspeaker(src)
 	case 11: // B - Write into B register
-		c.B = v
+		c.B = src
 	case 12: // XB - B = A + v*C
-		c.B = (c.A + v*c.C) & allBits
+		// TODO: Fix this
+		// Page 10 of "The last of the first" says that for the multiplier unit,
+		// numbers are signed 19-bit fractions. Is that the source,
+		// the C register, or both?
+		c.B = (c.A + src*c.C) & allBits
 	case 13: // L - If bit 20 is set, shift A and B left
-		if v.Sign() == 1 {
+		if src.Sign() == 1 {
 			c.A = (c.A << 1) & allBits
 			c.B = (c.B << 1) & allBits
 		}
 	case 14: // C - Write into C register
-		c.C = v
+		c.C = src
 	case 15: // PC - Add into C register
-		c.C = (c.C + v) & allBits
+		c.C = (c.C + src) & allBits
 	case 16: // SC - Subtract into C register
-		c.C = (c.C - v) & allBits
+		c.C = (c.C - src) & allBits
 	case 17: // n D - Write into a D register
-		c.D[k.Hi()] = v
+		c.D[inst.Hi()] = src
 	case 18: // n PD - Add into a D register
-		c.D[k.Hi()] = (c.D[k.Hi()] + v) & allBits
+		c.D[inst.Hi()] = (c.D[inst.Hi()] + src) & allBits
 	case 19: // n SD - Subtract into a D register
-		c.D[k.Hi()] = (c.D[k.Hi()] - v) & allBits
+		c.D[inst.Hi()] = (c.D[inst.Hi()] - src) & allBits
 	case 20: // Z - Null
 		// no-op I guess.
 	case 21: // HL - H as lower half
-		c.H = v.Lo()
+		c.H = src.Lo()
 	case 22: // HU - H as upper half
-		c.H = v.Hi()
+		c.H = src.Hi()
 	case 23: // S - Write into sequence register (absolute jump)
-		c.S = v
+		c.S = src
+		c.K = c.M[c.S]
 	case 24: // PS - Add into sequence register (relative jump)
-		c.S += v
+		c.S += src
+		c.K = c.M[c.S]
 	case 25: // CS - Count into sequence register
 		// TODO: check this
-		c.S += v.Ones()
+		c.S += src.Ones()
+		c.K = c.M[c.S]
 	case 26: // PK - Add into instruction register (add as upper half to next instruction)
 		// The "table of numbers" example is illuminating. For a program:
 		//     A PK
 		// 3 4 M B     // 3*32 + 4 = 100
 		// where A holds an index value into a table, PK then modifies the next
 		// instruction (3 4 M B, i.e. B = M[100]) to instead read from M[100+A].
-		c.K += (v << 10) & allBits
+		c.K = (c.K + src<<10) & allBits
 	case 27: // n MA - Disk 1
-		c.MA[k.Hi()] = v
+		c.MA[inst.Hi()] = src
 	case 28: // n MB - Disk 2
-		c.MB[k.Hi()] = v
+		c.MB[inst.Hi()] = src
 	case 29: // n MC - Disk 3
-		c.MC[k.Hi()] = v
+		c.MC[inst.Hi()] = src
 	case 30: // n MD - Disk 4
-		c.MD[k.Hi()] = v
+		c.MD[inst.Hi()] = src
 	case 31: // T - Stop if non-zero
-		if v != 0 {
+		if src != 0 {
 			return ErrStop
 		}
 	default:
